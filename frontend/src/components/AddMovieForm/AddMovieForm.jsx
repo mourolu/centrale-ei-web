7323388172a2a27d9f66cb90b292104bae58ccf2
Anicito Xavier <xavier.anicito@student-cs.fr>
2024-06-05 15:09:49 +0200

    Mise en place de la page pour ajouter un nouveau film.

--- frontend/src/components/AddMovieForm/AddMovieForm.jsx
+++ frontend/src/components/AddMovieForm/AddMovieForm.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react' ;
 import axios from 'axios' ;
-import './AddMovieForm.css' ;
+import './AddMovie.css' ;
 
-DEFAULT_FORM_VALUES = {name : '', dateOfRelease : '', image_url : '', genre_ids : [], adults : false, original_language : ''} ;
+const DEFAULT_FORM_VALUES = {name : '', dateOfRelease : '', image_url : '', genre_ids : [false, false, false], adults : false, original_language : ''} ;
 
 export default function add_movie({onSuccessfulMovieCreation}) {
 	const [formValues, setFormValues] = useState(DEFAULT_FORM_VALUES) ;
@@ -32,23 +32,32 @@ export default function add_movie({onSuccessfulMovieCreation}) {
 
 	return (
 		<div>
-			<form className = 'add_movie_form' onSubmit = {save}>
+			<form className = 'add_movie_form' onSubmit = {saveMovie}>
 				<input className = 'add_movie_input' required type = 'input' placeholder = 'title' value = {formValues.name} onChange = {(input) => setFormValues({ ...formValues, name : input.target.name})} />
 				<input className = 'add_movie_input' required type = 'date' placeholder = 'date_of_release' value = {formValues.dateOfRelease} onChange = {(input) => setFormValues({ ...formValues, dateOfRelease : input.target.dateOfRelease})} />
 				<input className = 'add_movie_input' required type = 'url' placeholder = 'title' value = {formValues.image_url} onChange = {(input) => setFormValues({ ...formValues, image_url : input.target.image_url})} />
-				<input className = 'add_movie_input' required type = 'input' placeholder = 'checkbox' value = {formValues.genre_ids} onChange = {(input) => setFormValues({ ...formValues, genre_ids : input.target.genre_ids})} />
-				<input className = 'add_movie_input' required type = 'input' placeholder = 'radio' value = {formValues.adults} onChange = {(input) => setFormValues({ ...formValues, adults : input.target.adults})} />
-				<input className = 'add_movie_input' required type = 'input' placeholder = 'original_language' value = {formValues.original_language} onChange = {(input) => setFormValues({ ...formValues, original_langage : input.target.original_language})} />
+				
+				<div>
+					<input className = 'add_movie_input' type = 'checkbox' id = 'genre_1' value = {formValues.genre_ids[0]} onChange = {(input) => setFormValues({ ...formValues, genre_ids : input.target.genre_ids[0]})} />
+					<label for = 'genre_1'>Genre 1</label>
+				</div>
+
+				<div>
+					<input className = 'add_movie_input' type = 'checkbox' id = 'adult' value = {formValues.adults} onChange = {(input) => setFormValues({ ...formValues, adults : input.target.adults})} />
+					<label for = 'adult'>Film pour adultes</label>
+				</div>
+
+				<input className = 'add_movie_input' required type = 'text' placeholder = 'original_language' value = {formValues.original_language} onChange = {(input) => setFormValues({ ...formValues, original_langage : input.target.original_language})} />
 				<button className = 'add_movie_button' type = 'submit'>Add movie</button>
 			</form>
 
 			{movieCreationSuccess !== null && (
 				<div className = 'movie_creation_success'>{movieCreationSuccess}</div>
-			)} ;
+			)}
 
 			{movieCreationError !== null && (
 				<div className = 'movie_creation_error'>{movieCreationError}</div>
-			)} ;
+			)}
 		</div>
 	) ;
 } ;
