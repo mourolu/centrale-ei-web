f1186efb02f0a0ce17e091242309410acf8a54f1
Anicito Xavier <xavier.anicito@student-cs.fr>
2024-06-06 16:18:50 +0200

    Modifications mineures.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,22 +6,22 @@ import About from './pages/About/About';
 import Layout from './components/Layout/Layout';
 import Counter from './pages/Counter/Counter';
 import Users from './pages/Users/Users';
-import AddMovie from './pages/Moveis/Movies' ;
+import AddMovie from './pages/Movies/Movies' ;
 import AboutMovie from './pages/AboutMovie/AboutMovie';
 
 function App() {
-  return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="counter" element={<Counter />} />
-        <Route path="users" element={<Users />} />
-        <Route path="about" element={<About />} />
-		<Route path = 'add-movie' element = {<AddMovie />} />
-        <Route path="film/:id" element={<AboutMovie />} />
-      </Routes>
-    </Layout>
-  );
+	return (
+	<Layout>
+		<Routes>
+			<Route path = "/" element = {<Home />} />
+			<Route path = "counter" element = {<Counter />} />
+			<Route path = "users" element = {<Users />} />
+			<Route path = "about" element = {<About />} />
+			<Route path = 'add-movie' element = {<AddMovie />} />
+			<Route path = "film/:id" element = {<AboutMovie />} />
+		</Routes>
+	</Layout>
+	);
 }
 
 export default App;
