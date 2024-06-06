81b9a3c496fb69636d38b7e0b8bb9a0ecc3b4885
Anicito Xavier <xavier.anicito@student-cs.fr>
2024-06-06 16:20:14 +0200

    Modifications mineures.

--- backend/migrations/1717682898741-$npm_config_name.js
+++ backend/migrations/1717682898741-$npm_config_name.js
@@ -2,8 +2,8 @@ import typeorm from "typeorm";
 
 const { MigrationInterface, QueryRunner } = typeorm;
 
-export default class  $npmConfigName1717680095653 {
-    name = ' $npmConfigName1717680095653'
+export default class  $npmConfigName1717682898741 {
+    name = ' $npmConfigName1717682898741'
 
     async up(queryRunner) {
         await queryRunner.query(`
@@ -66,6 +66,51 @@ export default class  $npmConfigName1717680095653 {
             SELECT "id",
                 "title",
                 "release_date"
+            FROM "movie"
+        `);
+        await queryRunner.query(`
+            DROP TABLE "movie"
+        `);
+        await queryRunner.query(`
+            ALTER TABLE "temporary_movie"
+                RENAME TO "movie"
+        `);
+        await queryRunner.query(`
+            CREATE TABLE "temporary_movie" (
+                "id" integer PRIMARY KEY NOT NULL,
+                "title" varchar NOT NULL,
+                "release_date" varchar NOT NULL,
+                "original_language" varchar NOT NULL,
+                "overview" varchar NOT NULL,
+                "poster_path" varchar NOT NULL,
+                "like" integer NOT NULL,
+                "adult" boolean NOT NULL,
+                "genre_ids" boolean NOT NULL,
+                CONSTRAINT "UQ_a81090ad0ceb645f30f9399c347" UNIQUE ("title"),
+                CONSTRAINT "UQ_57da1a996e8be977ad91814ea75" UNIQUE ("id")
+            )
+        `);
+        await queryRunner.query(`
+            INSERT INTO "temporary_movie"(
+                    "id",
+                    "title",
+                    "release_date",
+                    "original_language",
+                    "overview",
+                    "poster_path",
+                    "like",
+                    "adult",
+                    "genre_ids"
+                )
+            SELECT "id",
+                "title",
+                "release_date",
+                "original_language",
+                "overview",
+                "poster_path",
+                "like",
+                "adult",
+                "genre_ids"
             FROM "movie"
         `);
         await queryRunner.query(`
@@ -257,6 +302,50 @@ export default class  $npmConfigName1717680095653 {
         `);
         await queryRunner.query(`
             DROP TABLE "temporary_score"
+        `);
+        await queryRunner.query(`
+            ALTER TABLE "movie"
+                RENAME TO "temporary_movie"
+        `);
+        await queryRunner.query(`
+            CREATE TABLE "movie" (
+                "id" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+                "title" varchar NOT NULL,
+                "release_date" varchar NOT NULL,
+                "original_language" varchar NOT NULL,
+                "overview" varchar NOT NULL,
+                "poster_path" varchar NOT NULL,
+                "like" integer NOT NULL,
+                "adult" boolean NOT NULL,
+                "genre_ids" boolean NOT NULL,
+                CONSTRAINT "UQ_a81090ad0ceb645f30f9399c347" UNIQUE ("title")
+            )
+        `);
+        await queryRunner.query(`
+            INSERT INTO "movie"(
+                    "id",
+                    "title",
+                    "release_date",
+                    "original_language",
+                    "overview",
+                    "poster_path",
+                    "like",
+                    "adult",
+                    "genre_ids"
+                )
+            SELECT "id",
+                "title",
+                "release_date",
+                "original_language",
+                "overview",
+                "poster_path",
+                "like",
+                "adult",
+                "genre_ids"
+            FROM "temporary_movie"
+        `);
+        await queryRunner.query(`
+            DROP TABLE "temporary_movie"
         `);
         await queryRunner.query(`
             ALTER TABLE "movie"
